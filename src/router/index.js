c11835ff3eaa16cb318d298865b9a0c092c26c8e
wjnwjnwj <3323422990@qq.com>
2024-12-06 12:39:12 +0800

    transaction wjn

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router';
 import DataFlowPage from '../views/DataFlowPage.vue';
 import NextLevelPage from '../views/NextLevelPage.vue';
 import L1M2Page from '@/views/L1M2Page.vue';
+import L1M4Page from "@/views/L1M4Page.vue";
+import L2M4M1Page from "@/views/L2M4M1Page.vue";
 
 const routes = [
     { path: '/', component: DataFlowPage },
     { path: '/l1m2', component: L1M2Page },
+    { path: '/l1m4', component: L1M4Page },
+    { path: '/l2m4m1', component: L2M4M1Page },
     { path: '/next-level/:id', component: NextLevelPage }
 ];
 
