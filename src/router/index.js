bb8c1502b713b21d9fc6e4f58df40b21d1235b0d
wjnwjnwj <3323422990@qq.com>
2024-12-11 17:20:43 +0800

    阶段性胜利2

--- src/router/index.js
+++ src/router/index.js
@@ -11,16 +11,21 @@ import L2M2M2Page from '@/views/M2/L2M2M2Page.vue';
 import L2M2M3Page from '@/views/M2/L2M2M3Page.vue';
 import L2M2M4Page from '@/views/M2/L2M2M4Page.vue';
 import L2M3M1Page from '@/views/M3/L2M3M1Page.vue';
+import L2M4M1Page from "@/views/M4/L2M4M1Page.vue";
+import L2M4M2Page from "@/views/M4/L2M4M2Page.vue";
+import L2M4M3Page from "@/views/M4/L2M4M3Page.vue";
 import L3M2M3M1Page from '@/views/M2/L3M2M3M1Page.vue';
 import L3M2M3M2Page from '@/views/M2/L3M2M3M2Page.vue';
 import L3M2M3M3Page from '@/views/M2/L3M2M3M3Page.vue';
 import L3M3M1M1Page from '@/views/M3/L3M3M1M1Page.vue';
 import L3M3M1M2Page from '@/views/M3/L3M3M1M2Page.vue';
 import L3M3M1M3Page from '@/views/M3/L3M3M1M3Page.vue';
+import L3M4M1M1Page from "@/views/M4/L3M4M1M1Page.vue";
+import L3M4M1M2Page from "@/views/M4/L3M4M1M2Page.vue";
+import L3M4M2M1Page from "@/views/M4/L3M4M2M1Page.vue";
 import L4M2M3M2M1Page from '@/views/M2/L4M2M3M2M1Page.vue';
-import L2M4M1Page from "@/views/M4/L2M4M1Page.vue";
-import L2M4M2Page from "@/views/M4/L2M4M2Page.vue";
-import L2M4M3Page from "@/views/M4/L2M4M3Page.vue";
+import L4M4M1M2M1Page from "@/views/M4/L4M4M1M2M1Page.vue";
+
 
 const routes = [
     { path: '/', component: DataFlowPage },
@@ -43,7 +48,11 @@ const routes = [
     { path: '/l3m3m1m1', component: L3M3M1M1Page },
     { path: '/l3m3m1m2', component: L3M3M1M2Page },
     { path: '/l3m3m1m3', component: L3M3M1M3Page },
+    { path: '/l3m4m1m1', component: L3M4M1M1Page },
+    { path: '/l3m4m1m2', component: L3M4M1M2Page },
+    { path: '/l3m4m2m1', component: L3M4M2M1Page },
     { path: '/l4m2m3m2m1', component: L4M2M3M2M1Page },
+    { path: '/l4m4m1m2m1', component: L4M4M1M2M1Page },
     { path: '/next-level/:id', component: NextLevelPage }
 ];
 
